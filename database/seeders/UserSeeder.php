9622a8e01c9c928a19f8d7c5d2a489572e314ad9
danudiraja <danudiraja@gmail.com>
2024-05-23 10:03:33 +0700

    feat: Seeder User

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -20,13 +21,13 @@ class UserSeeder extends Seeder
                 'name' => 'Admin',
                 'email' => 'Admin@gmail.com',
                 'role' => '0',
-                'password' => 'admin123',
+                'password' => Hash::make('admin123'),
             ],
             [
                 'name' => 'Danu',
                 'email' => 'danu@gmail.com',
                 'role' => '1',
-                'password' => 'danu123',
+                'password' => Hash::make('danu123'),
             ],
         ];
 
